1dcc35021e980d5b201817fd772323bedd51d09e
devmatias <devmatias@gmail.com>
2022-06-14 10:00:05 -0300

    Initial Commit

---
+++ FirstAppRayBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirstAppRayBook
+//
+//  Created by Matias Correa Franco de Faria on 14/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstAppRayBook/FirstAppRayBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppRayBookApp.swift
+//  FirstAppRayBook
+//
+//  Created by Matias Correa Franco de Faria on 14/06/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppRayBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
